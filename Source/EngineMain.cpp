287e8e5612b372c1fa70d8ed6e6b1b7a0e01bb7b
Matthew Gordon <Toolz92@users.noreply.github.com>
2018-09-21 10:26:12 -0400

    Added virtual mem and storage Check
    Now displays Virtual Memory and Available storage on C: drive

--- Source/EngineMain.cpp
+++ Source/EngineMain.cpp
@@ -8,14 +8,24 @@ using namespace std;
 #define WIDTH 7
 
 int main() {
-	// Seems to display available RAM not HDD
+	//Displays AVailable RAM
 	MEMORYSTATUSEX statex;
 	statex.dwLength = sizeof(statex);
 	GlobalMemoryStatusEx(&statex);
-	double display = (statex.ullAvailPhys /DIV)/DIV;
-	cout << display << " Megabytes" << endl;
+	double displayRAM = (statex.ullAvailPhys /DIV)/DIV;
+	cout << "RAM: " << displayRAM << " Megabytes" << endl;
+	//Display Available Virual Mem
+	double displayVir = (statex.ullAvailVirtual / DIV) / DIV;
+	cout <<"Virtual Memory: "<< displayVir << " Megabytes" << endl;
 	//
 
+	//Display available Storage Space
+	ULARGE_INTEGER Freebytes;
+	unsigned long long QFree = 0;
+	GetDiskFreeSpaceEx(TEXT("C:\\"), &Freebytes, 0, 0);
+	QFree = Freebytes.QuadPart;
+	cout <<"Available Storage: " <<(QFree/DIV)/DIV << endl;
+	//
 	int q;
 	cin >> q;
 	return 0;
@@ -38,5 +48,21 @@ int WINAPI WinMain()
 	ReleaseMutex(hHandle); // Explicitly release mutex
 	CloseHandle(hHandle); // close handle before terminating
 	return(1);
+}
+*/
+/*
+bool CheckStorage(const DWORDLONG diskSpaceNeeded) {
+	// Check for enough free disk space on the current disk.
+	int const drive = _getdrive();
+	struct _diskfree_t diskfree;
+	_getdiskfree(drive, &diskfree);
+	unsigned __int64 const neededClusters;
+	neededClusters = diskSpaceNeeded /(diskfree.sectors_per_cluster *diskfree.bytes_per_sector);
+	if (diskfree.avail_clusters < neededClusters) {
+		// if you get here you don’t have enough disk space!
+		GCC_ERROR("CheckStorage Failure: Not enough physical storage.");
+		return false;
+	}
+	return true;
 }
 */
